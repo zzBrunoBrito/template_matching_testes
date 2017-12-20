ced52941085de77af60635319220084a5f81b646
Bruno Brito <bruno.brito.ln@gmail.com>
2017-12-20 16:00:02 -0300

    first commit

---
+++ ocr_note.py
@@ -0,0 +1,93 @@
+# USAGE
+# python ocr_template_match.py --image images/credit_card_01.png --reference ocr_a_reference.png
+
+# import the necessary packages
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to input image")
+args = vars(ap.parse_args())
+
+# initialize a rectangular (wider than it is tall) and square
+# structuring kernel
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
+# load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=900)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# apply a tophat (whitehat) morphological operator to find light
+# regions against a dark background (i.e., the credit card numbers)
+tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+
+
+
+# compute the Scharr gradient of the tophat image, then scale
+# the rest back into the range [0, 255]
+gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,
+                  ksize=-1)
+gradX = np.absolute(gradX)
+(minVal, maxVal) = (np.min(gradX), np.max(gradX))
+gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
+gradX = gradX.astype("uint8")
+
+# apply a closing operation using the rectangular kernel to help
+# cloes gaps in between credit card number digits, then apply
+# Otsu's thresholding method to binarize the image
+gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+thresh = cv2.threshold(gradX, 0, 255,
+                       cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+
+# apply a second closing operation to the binary image, again
+# to help close gaps between credit card number regions
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+locs = []
+
+# loop over the contours
+for (i, c) in enumerate(cnts):
+    # compute the bounding box of the contour, then use the
+    # bounding box coordinates to derive the aspect ratio
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w / float(h)
+
+    # since credit cards used a fixed size fonts with 4 groups
+    # of 4 digits, we can prune potential contours based on the
+    # aspect ratio
+    if ar > 1.0:
+        # contours can further be pruned on minimum/maximum width
+        # and height
+        if (w > 70 and w < 350) and (h > 10):
+            # append the bounding box region of the digits group
+            # to our locations list
+            locs.append((x, y, w, h))
+
+# sort the digit locations from left-to-right, then initialize the
+# list of classified digits
+locs = sorted(locs, key=lambda x: x[0])
+output = []
+
+# loop over the 4 groupings of 4 digits
+for (i, (gX, gY, gW, gH)) in enumerate(locs):
+    # initialize the list of group digits
+    groupOutput = []
+
+    # extract the group ROI of 4 digits from the grayscale image,
+    # then apply thresholding to segment the digits from the
+    # background of the credit card
+    group = gray[gY - 5:gY + gH + 5, gX - 5:gX + gW + 5]
+    print(gH)
+    print(gY)
+    cv2.imshow('img', group)
+    cv2.waitKey(0)
